b8b829fa08816c4592251ef2d8048a2fcfab4a0a
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-13 14:14:39 +0200

    chore: Update Vite configuration to include proxy for API requests

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,21 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-  },
-});
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+  return defineConfig({
+    plugins: [react()],
+
+    server: {
+      port: 3000,
+      proxy: {
+        "/api": {
+          target: `${process.env.VITE_API_URL}`,
+          changeOrigin: true,
+          secure: true,
+        },
+      },
+    },
+  });
+};
