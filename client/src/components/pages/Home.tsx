cf386f88fd17aed2c93815f96e06b49b76ba22a4
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-13 18:38:24 +0200

    feat: Add Button component and its variants

--- client/src/components/pages/Home.tsx
+++ client/src/components/pages/Home.tsx
@@ -1,7 +1,12 @@
+import { Button, buttonVariants } from "@atoms/button";
+
 const Home: React.FC = () => {
   return (
     <>
       <h1 className="text-lg font-medium text-blue-500">StrengthForge App</h1>
+      <Button className={buttonVariants({ variant: "outline" })}>
+        Click me
+      </Button>
     </>
   );
 };
