bf27ef1ec0164786ce9a65c5b5a4e126256e3b0f
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-14 22:18:16 +0200

    feat: Update router configuration for improved code organization

--- client/src/config/router.tsx
+++ client/src/config/router.tsx
@@ -1,10 +1,32 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import Home from "@pages/Home";
+import Home from "@pages/home";
+import Register from "@pages/register";
+import SignIn from "@pages/signIn";
+import AppLayout from "@templates/appLayout";
+import AuthLayout from "@templates/authLayout";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+  {
+    element: <AuthLayout />,
+    children: [
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <SignIn />,
+      },
+    ],
   },
 ]);
