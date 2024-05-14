b94545974714ba5adf19deb77a8b7496dc41a286
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-14 11:34:56 +0200

    feat: connection with db

--- server/go.mod
+++ server/go.mod
@@ -1,5 +1,10 @@
-module main
+module strength-forge-app
 
 go 1.22.2
 
-require github.com/rs/cors v1.11.0 // indirect
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/joho/godotenv v1.5.1
+	github.com/lib/pq v1.10.9
+	github.com/rs/cors v1.11.0
+)
--- server/main.go
+++ server/main.go
@@ -1,22 +1,74 @@
 package main
 
 import (
-	"encoding/json"
+	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
+	"os"
 
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/rs/cors"
 )
 
-type Message struct {
-	Text string `json:"text"`
+func GetEnvVariable(key string) (string, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return "", fmt.Errorf("error loading .env file: %w", err)
+	}
+	value := os.Getenv(key)
+	if value == "" {
+		return "", fmt.Errorf("environment variable %s not found", key)
+	}
+	return value, nil
+}
+
+var db *sql.DB
+
+func ConnectDB() (*sql.DB, error) {
+	dbName, err := GetEnvVariable("DB_NAME")
+	if err != nil {
+		return nil, err
+	}
+	dbUser, err := GetEnvVariable("DB_USER")
+	if err != nil {
+		return nil, err
+	}
+	dbPassword, err := GetEnvVariable("DB_PASSWORD")
+	if err != nil {
+		return nil, err
+	}
+	dbHost, err := GetEnvVariable("DB_HOST")
+	if err != nil {
+		return nil, err
+	}
+
+	connStr := fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable", dbUser, dbPassword, dbHost, dbName)
+	db, err := sql.Open("postgres", connStr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
 
 func main() {
+	var err error
 	mux := http.NewServeMux()
-
 	corsHandler := cors.Default().Handler(mux)
-	
+
+	db, err = ConnectDB()
+	if err != nil {
+		log.Fatalf("error connecting to database: %v", err)
+	}
+	defer db.Close()
+
 	log.Println("Server is running on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", corsHandler))
 }
