5efbed6d68bd8a690f13988ded5f41117897fcae
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-17 20:38:47 +0200

    refactor: Update signInForm component to use useAuthForm hook and auth_schemas

--- client/src/components/organisms/signInForm.tsx
+++ client/src/components/organisms/signInForm.tsx
@@ -1,10 +1,3 @@
-import { useNavigate } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { z } from "zod";
-import axios from "axios";
-import { useMutation } from "@tanstack/react-query";
-
 import {
   Form,
   FormItem,
@@ -14,64 +7,20 @@ import {
 } from "@molecules/form";
 import { Button } from "@atoms/button";
 import { Input } from "@atoms/input";
-import { useToast } from "@atoms/use-toast";
 
-interface SignInForm {
-  email: string;
-  password: string;
-}
+import useAuthForm from "@hooks/useAuthForm";
+import { signInschema } from "@schemas/auth_schemas";
+import { signInUser } from "@services/auth_services";
 
-const schema = z.object({
-  email: z
-    .string()
-    .min(3, "Name must be at least 3 characters long")
-    .max(100, "Name must be at most 100 characters long"),
-  password: z
-    .string()
-    .min(8, "Password must be at least 8 characters long")
-    .max(100, "Password must be at most 100 characters long"),
-});
-
-const logInUser = async (data: SignInForm) => {
-  const response = await axios.post("/api/auth/login", data);
-
-  return response.data;
-};
+import { SignInFormValues } from "@shared/form_types";
 
 const SignInForm = () => {
-  const navigate = useNavigate();
-  const { toast } = useToast();
-  const form = useForm({
-    resolver: zodResolver(schema),
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  const { mutate } = useMutation({
-    mutationFn: logInUser,
-    onSuccess: () => {
-      navigate("/");
-
-      toast({
-        title: "Success",
-        description: "You have successfully signed in",
-      });
-    },
-    onError: (error) => {
-      console.error(error);
-
-      toast({
-        title: "Error",
-        description: "Invalid email or password",
-        variant: "destructive",
-      });
-    },
-  });
-
-  const onSubmit = form.handleSubmit((data) => {
-    mutate(data);
+  const { form, onSubmit } = useAuthForm<SignInFormValues>({
+    schema: signInschema,
+    mutationFn: signInUser,
+    onSuccessRedirect: "/",
+    successMessage: "You have successfully signed in",
+    errorMessage: "Invalid email or password",
   });
 
   return (
