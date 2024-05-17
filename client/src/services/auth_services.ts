7aa846ad8291f584419ea809c7c6ae5a9a7ff82e
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-17 20:41:37 +0200

    refactor: Update auth_services to use API_ROUTES for authentication API endpoints

--- client/src/services/auth_services.ts
+++ client/src/services/auth_services.ts
@@ -1,13 +1,14 @@
 import axios from "axios";
 
 import { RegisterFormValues, SignInFormValues } from "@shared/form_types";
+import { API_ROUTES } from "@config/enums";
 
 export const signInUser = async (data: SignInFormValues) => {
-  const response = await axios.post("/api/auth/login", data);
+  const response = await axios.post(API_ROUTES.LOGIN, data);
   return response.data;
 };
 
 export const registerUser = async (data: RegisterFormValues) => {
-  const response = await axios.post("/api/auth/register", data);
+  const response = await axios.post(API_ROUTES.REGISTER, data);
   return response.data;
 };
