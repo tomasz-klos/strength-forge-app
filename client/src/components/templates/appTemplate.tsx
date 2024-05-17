2db616b6da04c6ee6119201aaf12bd86a4c554af
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-17 18:27:48 +0200

    refactor: Add Toaster component to app and auth templates

--- client/src/components/templates/appTemplate.tsx
+++ client/src/components/templates/appTemplate.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import { Toaster } from "@atoms/toaster";
+
 const AppTemplate = () => {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-950 text-zinc-50">
       <main className="flex-1">
         <Outlet />
       </main>
+      <Toaster />
     </div>
   );
 };
--- client/src/components/templates/authTemplate.tsx
+++ client/src/components/templates/authTemplate.tsx
@@ -1,11 +1,14 @@
 import { Outlet } from "react-router-dom";
 
+import { Toaster } from "@atoms/toaster";
+
 const AppTemplate = () => {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-950 text-zinc-50">
       <main className="flex-1 flex items-center justify-center px-4">
         <Outlet />
       </main>
+      <Toaster />
     </div>
   );
 };
