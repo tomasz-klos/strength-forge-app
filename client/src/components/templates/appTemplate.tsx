94a2548f51fcadce35cec95f65e0196b4933e865
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-17 19:54:35 +0200

    feat: Add Header component to AppTemplate

--- client/src/components/templates/appTemplate.tsx
+++ client/src/components/templates/appTemplate.tsx
@@ -2,13 +2,15 @@ import { Outlet, redirect } from "react-router-dom";
 import axios from "axios";
 
 import { Toaster } from "@atoms/toaster";
+import Header from "@organisms/header";
 
 const AppTemplate = () => {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-950 text-zinc-50">
-      <main className="flex-1">
+      <main className="flex-1 flex flex-col p-4 overflow-y-auto">
         <Outlet />
       </main>
+      <Header />
       <Toaster />
     </div>
   );
