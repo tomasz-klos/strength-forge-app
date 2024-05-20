a9309856dd1594cf9a917c77deffe6a5e8f0228d
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-20 16:16:09 +0200

    refactor: Update ValidateToken handler to check HTTP method and error handling

--- server/handlers/auth/validate_token_handler.go
+++ server/handlers/auth/validate_token_handler.go
@@ -1,21 +1,26 @@
 package handlers_auth
 
 import (
-	"log"
 	"net/http"
 )
 
 func (h *AuthHandler) ValidateToken(w http.ResponseWriter, r *http.Request) {
-	tokenString := r.Cookies()[0].Value
-	log.Println(tokenString)
-	if tokenString == "" {
+	if r.Method != http.MethodGet {
+		writeJSONResponse(w, http.StatusMethodNotAllowed, JSONResponse{Error: MsgMethodNotAllowed})
+		return
+	}
+
+	cookie, err := r.Cookie("token")
+	if err != nil || cookie.Value == "" {
 		writeJSONResponse(w, http.StatusUnauthorized, JSONResponse{Error: MsgNoTokenProvided})
 		return
 	}
 
+	tokenString := cookie.Value
+
 	user, err := h.service.ValidateToken(tokenString)
 	if err != nil {
-		log.Println(err)
+
 		writeJSONResponse(w, http.StatusUnauthorized, JSONResponse{Error: MsgUnauthorized})
 		return
 	}
