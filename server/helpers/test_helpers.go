a80bca404b881d9ee8eea38604a69ff9d5d2bd8d
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-20 16:11:05 +0200

    feat: Add test helper for executing HTTP requests and io reader

---
+++ server/helpers/test_helpers.go
@@ -0,0 +1,29 @@
+package helpers_test
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+type ErrorReader struct {
+}
+
+func (er *ErrorReader) Read(p []byte) (n int, err error) {
+	return 0, errors.New("read error")
+}
+
+func ExecuteRequest(t *testing.T, method, path string, body io.Reader, handler http.HandlerFunc) *httptest.ResponseRecorder {
+	req, err := http.NewRequest(method, path, body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	recorder := httptest.NewRecorder()
+	handler.ServeHTTP(recorder, req)
+
+	return recorder
+}
