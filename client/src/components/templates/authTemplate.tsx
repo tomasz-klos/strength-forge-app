80f9d8585a23a8222e6bffc8afef89e5eca8ed28
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-21 21:04:24 +0200

    refactor: Rename template and add type to loaders func

--- client/src/components/templates/authTemplate.tsx
+++ client/src/components/templates/authTemplate.tsx
@@ -3,7 +3,7 @@ import { Outlet, redirect } from "react-router-dom";
 import { Toaster } from "@atoms/toaster";
 import { validateToken } from "@services/auth_services";
 
-const AppTemplate: React.FC = () => {
+const AuthTemplate: React.FC = () => {
   return (
     <div className="flex flex-col min-h-screen bg-zinc-950 text-zinc-50">
       <main className="flex-1 flex items-center justify-center px-4">
@@ -14,9 +14,9 @@ const AppTemplate: React.FC = () => {
   );
 };
 
-export default AppTemplate;
+export default AuthTemplate;
 
-export const authTemplateLoader = async () => {
+export const authTemplateLoader = async (): Promise<null | Response> => {
   try {
     await validateToken();
 
