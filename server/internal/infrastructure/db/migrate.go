b936a8da58ab20ce6f0d612f17eab3899be4304a
Tomasz Klos <tomasz.klos@nerds.family>
2024-05-21 20:50:36 +0200

    refactor: Improve error handling in AutoMigrate function

--- server/internal/infrastructure/db/migrate.go
+++ server/internal/infrastructure/db/migrate.go
@@ -1,12 +1,14 @@
 package db
 
 import (
+	"log"
 	"strength-forge-app/internal/domain/models"
 )
 
 func AutoMigrate() {
 	err := DB.AutoMigrate(&models.User{})
+
 	if err != nil {
-		panic(err)
+		log.Fatalf("error migrating database: %v", err)
 	}
 }
